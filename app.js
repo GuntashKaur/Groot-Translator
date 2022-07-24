e8093057110d13bc6b70c7b2f2e9247e29d11d5b
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-07-24 13:43:30 +0530

    feat

---
+++ app.js
@@ -0,0 +1,26 @@
+var btnTranslate = document.querySelector("#btn");
+var  txtInput = document.querySelector("#textarea");
+var outputDiv = document.querySelector("#textarea2");
+
+
+function getTranslationURL(test){
+    return serverURL =  "https://api.funtranslations.com/translate/asian-accent.json" + "?" + "text=" + test;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something worng with server! try again after some time")
+}
+
+  btnTranslate.addEventListener("click", function clickEventHandler()
+  {
+    var inputText = txtInput.value; 
+    
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+  });
